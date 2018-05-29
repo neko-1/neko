f282b584007c3bf3c9c0289e547a935171c2d72d
liuzhihi <34497417+liuzhihi@users.noreply.github.com>
2018-05-29 10:59:34 +0800

    用户管理

--- user-manage/src/main/java/com/example/user/UserManageApplication.java
+++ user-manage/src/main/java/com/example/user/UserManageApplication.java
@@ -11,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UserManageApplication {
 
     public static void main(String[] args) {
+        SpringApplication s=new SpringApplication();
         SpringApplication.run(UserManageApplication.class, args);
     }
 }
