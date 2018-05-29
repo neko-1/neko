f70b0682f9474113d55316ecf4aa80321f300758
liuzhihi <34497417+liuzhihi@users.noreply.github.com>
2018-05-29 10:44:13 +0800

    用户管理

--- user-manage/src/main/java/com/example/user/UserManageApplication.java
+++ user-manage/src/main/java/com/example/user/UserManageApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @Author liuzhihai
+ * @Author lzh
  */
 
 @SpringBootApplication
